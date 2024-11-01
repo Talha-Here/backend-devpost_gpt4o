98191b92099b14a6e600bcde7fb3df6baf963d2c
TalhaHKhan <96955565+Talha-Here@users.noreply.github.com>
2024-11-01 06:33:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask_app import flask_app
 from flask_app_google_search import flask_app_google_search
 
 app = Flask(__name__)
-# CORS(app, origins=["http://localhost:3000"])
-CORS(app, origins=["https://devpost-gpt4o-hackathon.vercel.app"])
+CORS(app, origins=["http://localhost:3000"])
+# CORS(app, origins=["https://devpost-gpt4o-hackathon.vercel.app"])
 
 # Register both Flask apps with different prefixes
 app.register_blueprint(flask_app, url_prefix='/flask_app')
